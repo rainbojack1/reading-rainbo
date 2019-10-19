42ddc0e29c4d19fec7310d1036195e21de15fde7
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-10-18 22:49:39 -0400

    Create react app in client folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 const db = require("./models");
 const app = express();
 
