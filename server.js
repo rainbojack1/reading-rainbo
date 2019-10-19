bb8813ac96aaae38be02f433bddf217750596943
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-10-18 22:13:50 -0400

    Initial file creation and server setup.

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+// const db = require("./models");
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+    console.log("Mongoose connected.");
+});
+connection.on("error", (err) => {
+    console.log("Mongoose error: " + err);
+});
+
+app.use(express.static(__dirname + '/client/build'));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+    console.log(`App is running on http://localhost:${PORT}`);
+});
