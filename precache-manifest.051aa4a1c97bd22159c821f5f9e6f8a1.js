98a8cadda14b959435d83245ddab4b6f89c24363
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-10-20 00:04:54 -0400

    Updates

---
+++ precache-manifest.051aa4a1c97bd22159c821f5f9e6f8a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8443ebcda645bf4150673c0c3d925ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf92803a63d5763c84f6",
+    "url": "/static/css/main.df89cc09.chunk.css"
+  },
+  {
+    "revision": "a927477afbf5b8cb9308",
+    "url": "/static/js/2.d008ee9b.chunk.js"
+  },
+  {
+    "revision": "cf92803a63d5763c84f6",
+    "url": "/static/js/main.d8ae46ac.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
